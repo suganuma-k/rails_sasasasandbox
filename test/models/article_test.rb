c7c6ae7cda923cc0ecaad76c3d70fb9d2df59bef
suganuma-k <koki-s@weseek.co.jp>
2022-01-30 13:02:20 +0900

    Annotate article model, test and factory.

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 # frozen_string_literal: true
 
 require 'test_helper'
